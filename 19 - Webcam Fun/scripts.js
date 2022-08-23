f91ef4498951816fe57c289d9ea19f8f834e885f
robcc1527 <robcc1527@yahoo.com>
2022-08-22 19:53:28 -0500

    Created website

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,16 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo(){
+    navigator.mediaDevices.getUserMedia({video: true, aduio: false})
+    .then(localMediaStream =>{
+        console.log(localMediaStream);
+        video.src = window.URL.createObjectURL(localMediaStream);
+        video.play();
+    })
+    .catch(err => {
+        console.error(`OH No!!`, err);
+    });
+
+}
